60f011f72c26300967a1e5b04ff86a12cc983353
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-10-18 07:36:05 -0700

    Simple_shell

--- main.h
+++ main.h
@@ -1,28 +1,36 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
+
 #include <stdio.h>
-#include <stddef.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdbool.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-void execmd(char **argv);
-int execute_command(char **args);
-extern char **environ;
-size_t our_strlen(const char *str);
-ssize_t our_getline(char **line, size_t *len, FILE *stream);
-int our_strcmp(const char *string1, const char *string2);
-char *our_strdup(const char *str);
-char *our_strcpy(char *dest, char *src);
-char *our_strcat(char *final_str, char *str_to);
-void signal_handler(int signal);
 
-/* global variables */
+int lsh_ctrld(char **args);
+int lsh_cd(char **args);
+int lsh_help(char **args);
 extern char **environ;
+int lsh_exit(char **args);
+int _strcmp(char *s1, char *s2);
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+char *_get_path(char **env);
+int _values_path(char **arg, char **env);
+char *_getline_command(void);
+void _getenv(char **env);
+char **tokenize(char *lineptr);
+void _exit_command(char **args, char *lineptr, int _exit);
+int _fork_fun(char **arg, char **av, char **env,
+char *lineptr, int np, int c);
+
+
 #endif
